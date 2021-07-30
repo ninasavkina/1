a7c7aadc5b6140f3b36e7b62c48b145095b6ee04
Nina Savkina <nina_savkinaa@ukr.net>
2021-07-30 21:07:27 +0300

    add links

--- 2/src/App.js
+++ 2/src/App.js
@@ -1,45 +1,105 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import './App.css'
+import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
     },
 }));
 
+export default function SignIn() {
+    const classes = useStyles();
 
-function App() {
     return (
-        <>
-            <form action="" method="get">
-                <p title="Sing in">Sign in to your account</p>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
 
-                <TextField
-                    required
-                    id="filled-required"
-                    label="E-mail"
-                    variant="filled"
-                />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in to your account
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
 
-                <br/>
+                    <Grid item xs>
+                        <Link href="https://www.google.com" target="_blank" variant="body2">
+                            Forgot password?
+                        </Link>
+                    </Grid>
 
-                <TextField
-                    id="filled-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    variant="filled"
-                />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
 
-            </form>
-        </>
-);
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Stay signed in for a week"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        href="https://www.facebook.com"
+                        target="_blank"
+                    >
+                        Continue
+                    </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link href="https://www.google.com.ua/maps/" target="_blank" variant="body2">
+                                {"Use single sing-on (Google) instead"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
 }
-
-export default App;
