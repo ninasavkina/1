72963f13cb6a3e8c8e88b4f2839cb02ced962111
Nina Savkina <nina_savkinaa@ukr.net>
2021-07-30 18:53:12 +0300

    start

--- 2/src/App.js
+++ 2/src/App.js
@@ -4,9 +4,28 @@ import './App.css'
 
 function App() {
     return (
-        <Button variant="contained" color="primary">
-        Hello World
-    </Button>
+        <>
+            <form action="" method="get">
+                <p title="Sing in">Sign in to your account</p>
+
+                <div className="group">
+                    <label htmlFor="">E-mail</label>
+                    <input type="text" name="E-mail"/>
+                </div>
+
+                <div className="group">
+                    <label htmlFor="">Password:</label>
+                    <input type="password" name="password"/>
+                </div>
+
+                <div className="group">
+                    <Button>Регистрация</Button>
+                </div>
+
+                <a href="https://www.google.com" target="_blank">Forgot your password?</a>
+
+            </form>
+        </>
 );
 }
 
